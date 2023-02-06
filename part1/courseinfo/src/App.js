9eb3cc54845252c3ddae94a14a94fb08f156ce11
Olli Toivanen <olliteed@gmail.com>
2023-02-06 15:11:32 +0200

    Exercises1.1 and 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  const { parts } = props;
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const parts = [
+    { name: part1, exercises: exercises1 },
+    { name: part2, exercises: exercises2 },
+    { name: part3, exercises: exercises3 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
