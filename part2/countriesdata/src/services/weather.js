170d9d4fd973d4e64e1b42b302372662a5ab0ed2
Olli Toivanen <olliteed@gmail.com>
2023-03-27 16:08:21 +0300

    Clean part2 syntax

--- part2/countriesdata/src/services/weather.js
+++ part2/countriesdata/src/services/weather.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const getCityWeather = async (latlng) => {
-  console.log("latt", latlng);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=${process.env.REACT_APP_API_KEY}`;
   const request = axios.get(url);
   return request.then((res) => res.data);
