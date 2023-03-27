6cdfd90a42dee200ea7c68d5101a8b2da0c06ebe
Olli Toivanen <olliteed@gmail.com>
2023-03-27 16:56:24 +0300

    Add logger

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const { PORT } = require('./utils/config');
+const logger = require('./utils/logger');
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
