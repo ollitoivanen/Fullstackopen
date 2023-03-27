a3e16b797a02b25c6fff9348fa6b23178aa06f70
Olli Toivanen <olliteed@gmail.com>
2023-03-27 16:52:11 +0300

    Create app.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('./utils/config');
+const Blog = require('./models/blog');
+
+const mongoUrl = MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,35 +1,5 @@
-const http = require('http');
-const express = require('express');
-
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { MONGODB_URI, PORT } = require('./utils/config');
-const Blog = require('./models/blog');
-
-const mongoUrl = MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
+const app = require('./app');
+const { PORT } = require('./utils/config');
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
